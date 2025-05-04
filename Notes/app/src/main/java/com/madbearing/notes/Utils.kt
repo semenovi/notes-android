5a0c89a3dc09773e015237a09e95bfd5d17a431e
madbearing <t79601712352@gmail.com>
2025-05-05 02:15:29 +0300

    switch to maui

--- Notes/app/src/main/java/com/madbearing/notes/Utils.kt
+++
@@ -1,8 +0,0 @@
-package com.madbearing.notes
-
-import android.net.Uri
-
-fun getFileNameFromUri(uri: Uri): String {
-    val filePath = uri.path ?: return ""
-    return filePath.substring(filePath.lastIndexOf("/") + 1)
-}
